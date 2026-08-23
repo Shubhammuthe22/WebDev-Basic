58c6fb69f2891b2a5cb78d5db2d38cb2d69b4de2
Shubham Raosaheb Muthe <shubhammuthe@Shubhams-MacBook-Air.local>
2026-08-23 12:12:51 +0530

    Updated oop-part2

--- 06_JS-OOPS/oop-part2.js
+++ 06_JS-OOPS/oop-part2.js
@@ -28,3 +28,37 @@ let miniCalc = new Calculator();
 // 'miniCalc' cannot access the static method.
 console.log(Calculator.add(2,5));
 
+// Getters and Setters.
+// Get():- It is used to fetch the data from the variable
+// Set():- It is used to restrict the access to the variable or the object.
+
+class Employee{
+    #salary;
+    
+    constructor(name , salary){
+        if (salary < 0) {
+        throw new Error("Salary cannot be negative");
+        
+    } 
+        this.name = name;
+        this.#salary = salary;
+        // '_' in front of the salary is just used to indicate that the some special methods are coming.
+    }
+    get salary(){
+        return `You are not allowed to see the salary`;
+    }
+    set salary(value){
+        if (salary < 0) {
+            console.error("Invalid Salary");
+        } 
+        else {
+            this._salary = value;
+        }
+    }
+
+}
+let emp = new Employee("Alice",-50000);
+console.log(emp._salary);
+
+console.log(emp.salary);
+// get and set method puts the access restriction here. 
