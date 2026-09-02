69eeaa292a8ec81555fdbd92dea1b5d1aeae2779
Shubham Raosaheb Muthe <shubhammuthe@Shubhams-MacBook-Air.local>
2026-09-02 20:58:16 +0530

    Learned the Basics of Advanced JS

---
+++ 07_AdvancedJS/script.js
@@ -0,0 +1,14 @@
+console.log("Hello,You are on the Javascript File.");
+console.log(document.getElementsByTagName("h2"));
+// This is the example of DOM.
+
+
+// BOM(Browser Object Model):-
+// -It is used to manipulate the browser structure,environment and features. 
+// Following are the objects used in the BOM.
+// -window
+// -location
+// -Validator
+
+// DOM(Document Object Model):-
+// -
