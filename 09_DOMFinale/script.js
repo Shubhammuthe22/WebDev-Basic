9846cc6d4546b9365d8a29fe042f5161fa4b0814
Shubhammuthe22 <shubhammuthe355@gmail.com>
2026-09-07 22:31:55 +0530

    DOM finale completed

--- 09_DOMFinale/script.js
+++ 09_DOMFinale/script.js
@@ -5,3 +5,46 @@ document
 .addEventListener("dblclick",function (){
     alert("You Just Double clicked");
 });
+
+// example-7
+document
+.getElementById("carsList")
+.addEventListener("click",function(event){
+    if (event.target && event.target.matches(".listItems")) {
+        // Here we are adding the if statement to check whether both the conditions are satisfied or not.
+        alert("You selected" + event.target.textContent);
+    }
+    
+});
+
+// example 8
+
+document
+.getElementById("feedbackForm")
+.addEventListener('submit' ,function (event) {
+    event.preventDefault();
+    // It is used to change the default behaviour of the event for e.g Here is 'Submit'. 
+    let feedback = document.getElementById("feedbackInput").value;
+    console.log(feedback);
+    
+    let output = document.getElementById("formHandling");
+    // Get a reference to the element and then apply the CSS class.
+    output.classList.add("highlight");
+    output.textContent = (`Feedback is: ${feedback}`);
+    
+});
+
+// example 9
+
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("DOMstatus").textContent = ("DOM is fully Loaded");
+});
+
+// example 10
+document.getElementById("toggleButton")
+.addEventListener('click',function(){
+    let descriptionText = document.getElementById("descriptionText");
+    descriptionText.classList.add("highlight");
+    // toggle-Refers to the adding event on first click and removing it on the second click.
+    
+});
