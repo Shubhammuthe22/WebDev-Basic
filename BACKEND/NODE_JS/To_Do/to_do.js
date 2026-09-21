238f32845183c19079f33276aaea08049b67e541
Shubhammuthe22 <shubhammuthe355@gmail.com>
2026-09-21 20:38:49 +0530

    First Backend Challenge

--- BACKEND/NODE_JS/To_Do/to_do.js
+++ BACKEND/NODE_JS/To_Do/to_do.js
@@ -1,10 +1,20 @@
 const fs = require('fs');
 const filePath = "./tasks.json";
 
-loadTasks = () => {
+//flow of project-
+// grab the task(i/p)--> add the task to array --> save the task 
+
+
+const loadTasks = () => {
     try {
         const dataBuffer = fs.readFileSync(filePath);
+        //dataBuffer's data is in the complex form which cannot be understand easily,
+        // Hence it is required to be converted in the string format.
+
         const dataJSON = dataBuffer.toString();
+        // Example of raw JSON data
+        //"{\"name\":\"Alex\",\"age\":25,\"isDeveloper\":true}"
+
         return JSON.parse(dataJSON);
 
     } catch (error) {
@@ -12,8 +22,47 @@ loadTasks = () => {
     }
 }
 
+saveTask = (tasks) => {
+    const dataJSON = JSON.stringify(tasks);
+    fs.writeFileSync(filePath , dataJSON);
+}
 
+const addTask = (task) => {
+    const tasks = loadTasks();
+    tasks.push({task});
+    saveTask(tasks);
+    console.log("Task Added :" , task);
+    
+} 
 
+const listTask = () => {
+    const tasks = loadTasks();
+    tasks.forEach((task , index) => {
+        console.log(`${index + 1} : ${task.task}` );
+        
+    });
+}
+
+const removeTask = (argument) => {
+    const tasks = loadTasks();
+    const index = argument - 1;
+
+    if(index >= 0 && index < tasks.length){
+        tasks.splice(index , 1);
+        //This method removes 1 value at aprticular index.
+        console.log(`Task No ${index} removed`);
+        
+        saveTask(tasks);
+    }
+    else{
+        console.log("Invalid Task Number");
+        
+    }
+}
+
+const command = process.argv[2];
+const argument = process.argv[3]; // These both are used to access the value store in an array,
+                                // which are passed at the command line.
 
 if(command === 'add'){
     addTask(argument)
