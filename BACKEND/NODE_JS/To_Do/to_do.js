6694c76b55cbd0bf1f2dda9f4c777ec6151f1a90
Shubhammuthe22 <shubhammuthe355@gmail.com>
2026-09-21 18:02:03 +0530

    .

--- BACKEND/NODE_JS/To_Do/to_do.js
+++ BACKEND/NODE_JS/To_Do/to_do.js
@@ -1,6 +1,30 @@
 const fs = require('fs');
 const filePath = "./tasks.json";
 
+loadTasks = () => {
+    try {
+        const dataBuffer = fs.readFileSync(filePath);
+        const dataJSON = dataBuffer.toString();
+        return JSON.parse(dataJSON);
+
+    } catch (error) {
+        return []
+    }
+}
+
+
+
+
 if(command === 'add'){
-    addTask()
+    addTask(argument)
+}
+else if(command === 'list'){
+    listTask();
+}
+else if(command === 'remove'){
+    removeTask(parseInt(argument))
+}
+else{
+    console.log("Cannot find the command");
+    
 }
